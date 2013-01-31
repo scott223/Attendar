84eb4786bd0388742a9b37712fd58aaab02f5a54
Scott Brugmans <scott.brugmans@gmail.com>
2013-01-31 21:47:03 +0100

    added "register user"

--- ParseCloud/cloud/main.js
+++ ParseCloud/cloud/main.js
@@ -1,6 +1,53 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+//Parse.Cloud.define("hello", function(request, response) {
+//  response.success("Hello world!");
+//});
+
+Parse.Cloud.define("registerUser", function(request, response) {
+
+var currentUser = Parse.User.current();
+if (currentUser) {
+    
+    var authData = currentUser.get("authData");
+    
+ Parse.Cloud.httpRequest({
+  url: 'https://graph.facebook.com/' +authData.facebook.id,
+  params: {
+  	fields : 'id,name,email,gender,location,friends',
+    access_token : authData.facebook.access_token
+  },
+  success: function(httpResponse) {
+    resultaat = JSON.parse(httpResponse.text);
+    
+	currentUser.set("fbFriendsArray",resultaat.friends.data);
+	currentUser.set("email",resultaat.email);
+	currentUser.set("name",resultaat.name);
+	currentUser.set("gender",resultaat.gender);
+	currentUser.set("location",resultaat.location.name);
+	currentUser.set('fbID',parseInt(resultaat.id));
+	currentUser.save(null, {
+  success: function(currentUser) {
+    response.success('200');
+  },
+  error: function(currentUser, error) {
+    response.error(error);
+  }
 });
+
+  },
+  error: function(httpResponse) {
+    console.error('Request failed with response code ' + httpResponse.status);
+  }
+});
+    
+ 
+ } else {
+  
+  response.success('user not found');
+  
+  }
+  
+});
+
